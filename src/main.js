b06deef85bb975b4e9ab6e2fb352e9f6ab27ab02
Volodymyr Mazun <mebelmv1990@gmail.com>
2025-06-28 14:09:33 +0300

    create new page

--- src/main.js
+++ src/main.js
@@ -3,6 +3,3 @@ import './js/gallery';
 import './js/FAQ';
 import './js/coockiesFile';
 import './js/Testimonials';
-import './js/footer-privacy.js';
-import './js/footer-terms.js';
-import './js/footer-cookies';
